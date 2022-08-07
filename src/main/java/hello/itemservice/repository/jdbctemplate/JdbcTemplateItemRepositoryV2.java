6b1d640bc12aa8f7b466e7dfd9faf82328b7ea26
JinHyunTaek <jht2512@naver.com>
2022-08-07 22:19:20 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
@@ -0,0 +1,113 @@
+package hello.itemservice.repository.jdbctemplate;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.util.*;
+
+@Slf4j
+public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
+
+    private final NamedParameterJdbcTemplate template;
+
+    public JdbcTemplateItemRepositoryV2(DataSource dataSource) {
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Item save(Item item) {
+        String sql = "insert into item(item_name,price,quantity)" +
+                " values (:itemName, :price, :quantity)";
+
+        SqlParameterSource param = new BeanPropertySqlParameterSource(item);
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        template.update(sql, param, keyHolder);
+
+        Long key = keyHolder.getKey().longValue();
+        item.setId(key);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        String sql = "update item" +
+                " set item_name=:itemName, price=:price, quantity=:quantity" +
+                " where id =:id";
+
+        SqlParameterSource mapSqlParameterSource = new MapSqlParameterSource()
+                .addValue("itemName", updateParam.getItemName())
+                .addValue("price", updateParam.getPrice())
+                .addValue("quantity", updateParam.getQuantity())
+                .addValue("id", itemId);
+
+        template.update(sql, mapSqlParameterSource);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        String sql = "select id,item_name,price,quantity from item where id=:id";
+        try {
+            Map<String,Object> param = Map.of("id",id);
+            //queryForObject: 단일 데이터 가져올 때
+            Item item = template.queryForObject(sql, param, itemRowMapper());
+            return Optional.of(item); //require item is not null
+        }catch (EmptyResultDataAccessException e){
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        SqlParameterSource param = new BeanPropertySqlParameterSource(cond);
+
+        String sql = "select id,item_name,price,quantity from item";
+
+        //동적 쿼리
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            sql += " where";
+        }
+        boolean andFlag = false;
+
+        if (StringUtils.hasText(itemName)) {
+            sql += " item_name like concat('%',:itemName,'%')";
+            andFlag = true;
+        }
+        if (maxPrice != null) {
+            if (andFlag) {
+                sql += " and";
+            }
+            sql += " price <= :maxPrice";
+        }
+
+        // template.query: list 데이터 가져올 때
+        log.info("sql={}",sql);
+        return template.query(sql,param,itemRowMapper());
+    }
+
+    //RowMapper<T> 는 데이터베이스의 결과인 ResultSet 을 반환한다.
+    // (JdbcTemplate 이 ResultSet 이 끝날 때까지 while 또는 if 문을 (개발자 대신) 돌려서 객제로 반환
+    private RowMapper<Item> itemRowMapper(){
+        return BeanPropertyRowMapper.newInstance(Item.class);
+    }
+
+}
